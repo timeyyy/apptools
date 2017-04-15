0a0242acd2750c78fb5261f8f3aa4b9cc63ed75c
timeyyy <timeyyy_da_man@hotmail.com>
2017-04-15 11:38:32 +0200

    update version 0.1.3

--- setup.py
+++ setup.py
@@ -1,15 +1,9 @@
 from setuptools import setup, find_packages # Always prefer setuptools over distutils
-from codecs import open 					# To use a consistent encoding
 from os import path
-here = path.abspath(path.dirname(__file__))
-# Get the long description from the relevant file
-with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
-	long_description = f.read()
 setup(
 	name = 'peasoup',
-	version='0.1.2',
+	version='0.1.3',
 	description = 'Common tools for application deployment!',
-	long_description =long_description,
 	url = 'https://github.com/timeyyy/apptools',
 	author='timothy eichler',
 	author_email='tim_eichler@hotmail.com',
