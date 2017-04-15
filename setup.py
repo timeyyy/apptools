d3c0f324b0c2689c35f5601348276f4efd6cb240
timeyyy <timeyyy_da_man@hotmail.com>
2017-04-15 18:38:33 +0200

    setup.py: add install_requires and version 0.1.5

--- setup.py
+++ setup.py
@@ -2,19 +2,25 @@ from setuptools import setup, find_packages # Always prefer setuptools over dist
 from os import path
 setup(
 	name = 'peasoup',
-	version='0.1.4',
+	version='0.1.5',
 	description = 'Common tools for application deployment!',
 	url = 'https://github.com/timeyyy/apptools',
 	author='timothy eichler',
 	author_email='tim_eichler@hotmail.com',
 	license='BSD3',
 	classifiers=[
-		'Development Status :: 3 - Alpha',
-		'Intended Audience :: Developers',
-		'Topic :: Software Development :: Build Tools',
-		'License :: OSI Approved :: BSD License',
-		'Programming Language :: Python :: 3',
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Build Tools',
+            'License :: OSI Approved :: BSD License',
+            'Programming Language :: Python :: 3',
 	],
+        install_requires = [
+            'tkquick',
+            'psutil',
+            'appdirs',
+            'timstools'
+        ],
 
 	keywords = 'gui programming apptools python3 permissions deployment application',
 
